b87a0cceea189e9a31fd1dcc6e3334ffdf8a828c
ywg228 <1107779144@qq.com>
2016-07-30 17:11:29 +0800

    commit

--- VerticalGuideActivity/app/src/main/java/com/ywg/verticalguide/VerticalGuideActivity.java
+++ VerticalGuideActivity/app/src/main/java/com/ywg/verticalguide/VerticalGuideActivity.java
@@ -1,28 +1,64 @@
 package com.ywg.verticalguide;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
- * 垂直滑动引导页
+ * 视差效果的垂直滑动引导页
  */
 public class VerticalGuideActivity extends AppCompatActivity {
 
     private static final float MIN_SCALE = 0.75f;
     private static final float MIN_ALPHA = 0.75f;
 
+    private Button mBtnEnterMain;
+
+    private VerticalViewPager mVerticalViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vertical_guide);
-        VerticalViewPager verticalViewPager = (VerticalViewPager) findViewById(R.id.vertical_viewpager);
+        mBtnEnterMain = (Button) findViewById(R.id.btn_enter_main);
 
-        verticalViewPager.setAdapter(new DemoAdapter(getSupportFragmentManager()));
-        verticalViewPager.setPageMargin(0);
+        mVerticalViewPager = (VerticalViewPager) findViewById(R.id.vertical_viewpager);
+
+        mVerticalViewPager.setAdapter(new DemoAdapter(getSupportFragmentManager()));
+        mVerticalViewPager.setPageMargin(0);
         //verticalViewPager.setPageMarginDrawable(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
 
-        verticalViewPager.setPageTransformer(true, new ParallaxTransformer(R.id.view_scroller));
-    
+        mVerticalViewPager.setPageTransformer(true, new ParallaxTransformer(R.id.view_scroller));
+
+        mVerticalViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position == 2) {
+                    mBtnEnterMain.setVisibility(View.VISIBLE);
+                }else {
+                    mBtnEnterMain.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mBtnEnterMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 }
